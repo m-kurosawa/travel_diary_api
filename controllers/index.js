4c34bc46c3500a97e64ef1adfb9654cf049bcd44
m-kurosawa <michiki.kurosawa@gmail.com>
2021-11-11 16:27:34 +0900

    [add] user api

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ module.exports = (models) => {
         res.status(200).send("OK");
     }
 
-    const getUser = (req, res) => {
+    const getUsers = (req, res) => {
         models.users
             .list()
             .then((users) => users.map((user) => user.serialize()))
@@ -15,8 +15,27 @@ module.exports = (models) => {
             .catch((err) => res.status(400).send(err.message));
     }
 
+    const getUser = (req, res) => {
+        const { username } = req.params;
+        models.users
+            .get({ username })
+            .then((user) => res.json(user))
+            .catch((err) => res.status(404).send(err.message));
+    }
+
+    const putUser = (req, res) => {
+        const { username } = req.body;
+        const sql_query = { username: username };
+        models.users
+            .create(sql_query)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(400).send(err.message));
+    }
+
     //request list
     router.get("/test", testAPI);
-    router.get("/users", getUser);
+    router.get("/users", getUsers);
+    router.get("/user/:username", getUser)
+    router.put("/user", putUser)
     return router;
 };
